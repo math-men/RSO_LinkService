4b09980198a43da07b7d8e47bcfe898c796c40a8
Paweł Walczak <walczakp007@gmail.com>
2019-05-12 23:14:52 +0200

    Insert data

--- dbConn/prepareData.go
+++ dbConn/prepareData.go
@@ -10,6 +10,89 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
+type Link struct {
+	Owner     string      `json:"owner"`
+	Original  string      `json:"original"`
+	Processed string			`json:"processed"`
+	Cost      int			    `json:"cost"`
+}
+
+func createTable(svc *dynamodb.DynamoDB) {
+	input := &dynamodb.CreateTableInput{
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("owner"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("original"),
+				AttributeType: aws.String("S"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("owner"),
+				KeyType:       aws.String("HASH"),
+			},
+			{
+				AttributeName: aws.String("original"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+			ReadCapacityUnits:  aws.Int64(10),
+			WriteCapacityUnits: aws.Int64(10),
+		},
+		TableName: aws.String("Links"),
+	}
+
+	result, err := svc.CreateTable(input)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(result)
+	fmt.Println("Successfully created dynamoDB data model")
+}
+
+func insertData(svc *dynamodb.DynamoDB) {
+	linksData, err := os.Open("linkdata.json")
+	defer linksData.Close()
+	if err != nil {
+		fmt.Println("Could not open the moviedata.json file", err.Error())
+		os.Exit(1)
+  }
+	var links []Link
+	err = json.NewDecoder(linksData).Decode(&links)
+	if err != nil {
+		fmt.Println("Could not decode the linksData.json data", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(links)
+
+	for _, link := range links {
+
+		info, err := dynamodbattribute.MarshalMap(link)
+		if err != nil {
+			panic(fmt.Sprintf("failed to marshal the link, %v", err))
+		}
+
+		input := &dynamodb.PutItemInput{
+			Item:      info,
+			TableName: aws.String("Links"),
+		}
+
+		_, err = svc.PutItem(input)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+	}
+
+	fmt.Printf("We have processed %v records\n", len(links))
+}
+
 func main() {
 	fmt.Println("Starting configuration of data model")
 	config := &aws.Config{
@@ -21,81 +104,7 @@ func main() {
 
 	svc := dynamodb.New(sess)
 
-	input := &dynamodb.CreateTableInput{
-		AttributeDefinitions: []*dynamodb.AttributeDefinition{
-			{
-				AttributeName: aws.String("year"),
-				AttributeType: aws.String("N"),
-			},
-			{
-				AttributeName: aws.String("title"),
-				AttributeType: aws.String("S"),
-			},
-		},
-		KeySchema: []*dynamodb.KeySchemaElement{
-			{
-				AttributeName: aws.String("year"),
-				KeyType:       aws.String("HASH"),
-			},
-			{
-				AttributeName: aws.String("title"),
-				KeyType:       aws.String("RANGE"),
-			},
-		},
-		ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
-			ReadCapacityUnits:  aws.Int64(10),
-			WriteCapacityUnits: aws.Int64(10),
-		},
-		TableName: aws.String("Movies"),
-	}
-
-	result, err := svc.CreateTable(input)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-	fmt.Println(result)
-	fmt.Println("Successfully created dynamoDB data model")
-
-	type Movie struct {
-		Year  int         `json:"year"`
-		Title string      `json:"title"`
-	}
-
-	moviesData, err := os.Open("moviedata.json")
-	defer moviesData.Close()
-	if err != nil {
-		fmt.Println("Could not open the moviedata.json file", err.Error())
-		os.Exit(1)
-  }
-	var movies []Movie
-	err = json.NewDecoder(moviesData).Decode(&movies)
-	if err != nil {
-		fmt.Println("Could not decode the moviedata.json data", err.Error())
-		os.Exit(1)
-	}
-	fmt.Println(movies)
-
-	for _, movie := range movies {
-
-		info, err := dynamodbattribute.MarshalMap(movie)
-		if err != nil {
-			panic(fmt.Sprintf("failed to marshal the movie, %v", err))
-		}
-
-		input := &dynamodb.PutItemInput{
-			Item:      info,
-			TableName: aws.String("Movies"),
-		}
-
-		_, err = svc.PutItem(input)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-	}
-
-	fmt.Printf("We have processed %v records\n", len(movies))
+	createTable(svc)
+	insertData(svc)
 
 }
